3768a9fd667bea864d5d8468734fff47706ce208
Monu Kenvat <monukevat3@gmail.com>
2024-11-16 09:42:29 +0530

    report chanfes

--- Preowned Car Management System/DashBoardForm.cs
+++ Preowned Car Management System/DashBoardForm.cs
@@ -19,10 +19,10 @@ namespace Preowned_Car_Management_System
         long id;
         bool manager = false;
 
-        public static String connectionString = "Data Source=LAPTOP-Q6RR7BFH\\SQLEXPRESS08;Initial Catalog=PreOwnedCarManagementDatabase;Integrated Security=True;";
+        //public static String connectionString = "Data Source=LAPTOP-Q6RR7BFH\\SQLEXPRESS08;Initial Catalog=PreOwnedCarManagementDatabase;Integrated Security=True;";
      
 
-       // public static String connectionString = "Data Source=MONU\\SQLEXPRESS;Initial Catalog=PreOwnedCarManagementDatabase;Integrated Security=True;TrustServerCertificate=True";
+        public static String connectionString = "Data Source=MONU\\SQLEXPRESS;Initial Catalog=PreOwnedCarManagementDatabase;Integrated Security=True;TrustServerCertificate=True;";
         public DashBoardForm(Form loginForm,long id)
         {
             this.loginForm = loginForm;
@@ -294,6 +294,11 @@ namespace Preowned_Car_Management_System
         private void panel17_Click_1(object sender, EventArgs e)
         {
             SeeProfile();
+        }
+
+        private void panel15_Paint_1(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
